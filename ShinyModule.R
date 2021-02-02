029afeefa35d963684a88ba4e160968669557f14
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-02-02 12:30:05 +0100

    add pdf artefact output

--- ShinyModule.R
+++ ShinyModule.R
@@ -25,6 +25,11 @@ shinyModuleConfiguration <- function(id, input) {
 shinyModule <- function(input, output, session, data, col, num) {
   current <- reactiveVal(data)
 
+  map1 <- get_map(bbox(extent(data)+c(-num,num,-num,num)))
+  pdfmap <- ggmap(map1) +
+    geom_path(data=as.data.frame(data),aes(x=location_long,y=location_lat,group=local_identifier),colour=col)
+  ggsave(pdfmap, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"openstreetmap.pdf"))
+  
   osmap <- reactive({
     map <- get_map(bbox(extent(data)+c(-input$num,input$num,-input$num,input$num)))
   
