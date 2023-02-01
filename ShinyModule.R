98076c81a9a3c6829e63362b55ac56497e7852cd
annescharf <ascharf@ab.mpg.de>
2023-02-01 13:09:46 +0100

    updated code and files

--- ShinyModule.R
+++ ShinyModule.R
@@ -1,53 +1,39 @@
 library('move')
 library('ggmap')
 library('shiny')
+library("randomcoloR")
+library( "shinyWidgets")
+library("shinycssloaders")
+library("scales")
 
-shinyModuleUserInterface <- function(id, label, num, col) {
+## choose a better color selection?....
+
+shinyModuleUserInterface <- function(id, label) {
   ns <- NS(id)
-
+  
   tagList(
     titlePanel("Open Street map"),
-    numericInput(ns("num"), "Define edge size to view coastlines (in degrees of lon/lat):", num, min = 0, max = 90,step=0.00001),
-    verbatimTextOutput("value1"),
-    plotOutput(ns("map"),height="90vh")
+    numericInput(ns("num"), "Define edge size to view coastlines (in degrees of lon/lat):", value=0.01, min = 0, max = 90,step=0.00001),
+    withSpinner(plotOutput(ns("map"),height="80vh"))
   )
 }
 
-shinyModuleConfiguration <- function(id, input) {
-  ns <- NS(id)
-  
-  configuration <- list()
-  
-  configuration
-}
 
-shinyModule <- function(input, output, session, data, col, num) {
+shinyModule <- function(input, output, session, data) {
   current <- reactiveVal(data)
-
-  if(length(grep(",",col))>0)
-  {
-    cols <- trimws(strsplit(col,",")[[1]])
-    colspt <- rep(cols,n.locs(data))
-  } else colspt <- col
   
-  map1 <- get_map(bbox(extent(data)+c(-num,num,-num,num)))
-  pdfmap <- ggmap(map1) +
-    geom_path(data=as.data.frame(data),aes(x=location_long,y=location_lat,group=local_identifier),colour=colspt) +
-    geom_point(data=as.data.frame(data),aes(x=location_long,y=location_lat),colour=colspt,size=3)
-    
-  ggsave(pdfmap, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"openstreetmap.pdf"))
-  
-  osmap <- reactive({
-    map <- get_map(bbox(extent(data)+c(-input$num,input$num,-input$num,input$num)))
-  
-    out <- ggmap(map) +
-      geom_path(data=as.data.frame(data),aes(x=location_long,y=location_lat,group=local_identifier),colour=colspt) +
-      geom_point(data=as.data.frame(data),aes(x=location_long,y=location_lat),colour=colspt,size=3)
-    out
-    })
-
   output$map <- renderPlot({
-    osmap()
+    mDF <- data.frame(long=coordinates(data)[,1],lat=coordinates(data)[,2], indiv=trackId(data))
+    n <- length(unique(mDF$indiv))
+    if(n < 10){colspt <- brewer_pal(palette = "Set1")(n)}else{colspt <- distinctColorPalette(n)}
+    
+    map <- get_map(bbox(extent(data)+c(-input$num,input$num,-input$num,input$num)),source="stamen") #,source="osm")
+    osmap <- ggmap(map) +
+      geom_path(data=mDF,aes(x=long,y=lat,group=indiv,colour=indiv)) +
+      geom_point(data=mDF,aes(x=long,y=lat,colour=indiv),size=3)+
+      scale_color_manual("Individuals",values=colspt)
+    ggsave(osmap, file = appArtifactPath("openstreetmap.pdf"))
+    osmap
   })
   
   return(reactive({ current() }))
