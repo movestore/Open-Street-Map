d55c2c25d4a46b3c65665c4a15a9b3d67a13855e
annescharf <ascharf@ab.mpg.de>
2024-09-19 10:32:10 +0200

    update to move2

--- ShinyModule.R
+++ ShinyModule.R
@@ -1,23 +1,41 @@
-library('move')
-library('ggmap')
+library('move2')
+library('ggspatial')
+library("ggplot2")
 library('shiny')
 library("randomcoloR")
 library("shinycssloaders")
 library("scales")
+library("shinyBS")
 
 ## choose a better color selection?....
 
+# rosm::osm.types()
+# [1] "osm" "hotstyle"   "cartodark" "cartolight"   
+# api req  "opencycle" "osmtransport" "thunderforestlandscape" "thunderforestoutdoors"
+# not very inform "loviniahike"  "loviniacycle"  
+# problems "stamenbw" "stamenwatercolor"
+
 shinyModuleUserInterface <- function(id, label) {
   ns <- NS(id)
-  
-  tagList(
-    titlePanel("Open Street map"),
-    numericInput(ns("num"), "Define edge size to view coastlines (in degrees of lon/lat):", value=1, min = 0, max = 90,step=0.00001),
-    sliderInput(inputId = ns("zoom"), 
-                label = "Resolution of background map", 
-                value = 5, min = 3, max = 18, step=1),
-    textInput(ns("api"), "Enter your stadia API key. (This is required until MoveApps provides its OSM mirror. Register with stamen, it is free: https://stadiamaps.com/stamen/onboarding/create-account", value = ""),
-    withSpinner(plotOutput(ns("map"),height="80vh"))
+  fluidPage(
+    titlePanel("Open Street Map"),
+    sidebarLayout(
+      sidebarPanel(
+        checkboxInput(ns("legend"), label = "Show ledgend with track names", value = FALSE),
+        radioButtons(ns("maptype"),label = "Background map types",
+                     choices = list("OSM" = "osm", "Humanitarian" = "hotstyle", "Carto Dark" = "cartodark", "Carto Light" = "cartolight"),
+                     selected = "osm" ),
+        bsTooltip(id=ns("maptype"), title="Inspect the different map types at a low resolution as it is faster", placement = "bottom", trigger = "hover", options = list(container = "body")),
+        
+        radioButtons(ns("zoomin"),label = "Resolution of background map",
+                     choices = list("very low" = -2, "low" = -1, "high" = 0, "very high" = 1),
+                     selected = -2 ),
+        bsTooltip(id=ns("zoomin"), title="Chosing a very high resolution for data covering a large area might lead the App to crash", placement = "bottom", trigger = "hover", options = list(container = "body"))
+      ,width = 2),
+      mainPanel(
+        withSpinner(plotOutput(ns("map"),height="80vh"))
+      , width = 10)
+    )
   )
 }
 
@@ -26,19 +44,23 @@ shinyModule <- function(input, output, session, data) {
   current <- reactiveVal(data)
   
   output$map <- renderPlot({
-    mDF <- data.frame(long=coordinates(data)[,1],lat=coordinates(data)[,2], indiv=trackId(data))
-    n <- length(unique(mDF$indiv))
-    if(n < 10){colspt <- brewer_pal(palette = "Set1")(n)}else{colspt <- distinctColorPalette(n)}
+    osmap <- ggplot() +
+      ggspatial::annotation_map_tile(zoomin = as.numeric(input$zoomin), type=input$maptype) +
+      ggspatial::annotation_scale(aes(location="br")) +
+      theme_linedraw() +
+      geom_sf(data = mt_track_lines(data),  aes(color = !!id_col)) +
+      geom_sf(data = data, aes(color = !!id_col), size = 1)
     
-    if(input$api=="") logger.info("no API key entered") else register_stadiamaps(input$api)
+    if(!input$legend){
+      mp <- osmap +guides(color = "none")
+      
+    }
+    if(input$legend){
+      mp <- osmap + scale_color_manual("Tracks",values=colspt)
+    }
     
-    map <- get_stadiamap(bbox(extent(data)+c(-input$num,input$num,-input$num,input$num)),source="stamen_terrain",zoom=input$zoom)
-    osmap <- ggmap(map) +
-      geom_path(data=mDF,aes(x=long,y=lat,group=indiv,colour=indiv)) +
-      geom_point(data=mDF,aes(x=long,y=lat,colour=indiv),size=3)+
-      scale_color_manual("Individuals",values=colspt)
-    ggsave(osmap, file = appArtifactPath("openstreetmap.pdf"))
-    osmap
+    ggsave(mp, file = appArtifactPath("openstreetmap.pdf"))
+    mp
   })
   
   return(reactive({ current() }))
